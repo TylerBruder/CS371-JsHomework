a84c63247b680c05dbe4ba475a712f8985cbbb20
Tyler Bruder <brudert@mail.gvsu.edu>
2019-02-11 11:58:15 -0500

    Finished hw3 JS file

--- hw3-functions.js
+++ hw3-functions.js
@@ -2,6 +2,7 @@
 /**
  * Created by Hans Dulimarta
  * TODO: Add your name below this line
+ * @author Tyler Bruder
  */
 
 /**
@@ -15,8 +16,17 @@
  * @returns {number}
  */
 function findElementsWithId(rootId, klazName) {
-  /* complete this function */
-  return 0;
+  var numElements = 0;
+  const klaz = document.getElementById(rootId);
+  var child = klaz.childNodes;
+
+  for (var i = 0; i < child.length; i++) {
+    if (child[i].getAttribute("id") != null) {
+      child[i].className = klazName;
+      numElements++;
+    }
+  }
+  return numElements;
 }
 
 /**
@@ -27,5 +37,37 @@ function findElementsWithId(rootId, klazName) {
  * @returns NONE
  */
 function createTable() {
-  /* complete this function */
+  //logic from https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Traversing_an_HTML_table_with_JavaScript_and_DOM_Interfaces
+  const quote = new LoremIpsum();
+  const info = document.getElementsByClassName("table-home");
+  //getting given number of rows
+  var numRows = info[0].getAttribute("data-gv-row");
+  //adding 1 at function call so its never null 
+  numRows++;
+  //getting given number of columns
+  var numcols = info[0].getAttribute("data-gv-column");
+
+  var newTable = document.createElement("table");
+  var body = document.createElement("table");
+  newTable.appendChild(body);
+
+  for (var i = 0; i < numRows; i++) {
+    //creates number of rows in new table based off random value given
+    var tr = document.createElement("tr");
+    body.appendChild(tr);
+  }
+
+  for (var j = 0; j < numcols; j++) {
+    if (i == 0) {
+      var header = document.createElement("th");
+      tr.appendChild(header);
+    }
+    else {
+      var td = document.createElement("td");
+      let quotes = quote.generate(4);
+      td.appendChild(document.createTextNode(quotes));
+      tr.appendChild(td);
+    }
+  }
+  info[0].appendChild(body);
 }
